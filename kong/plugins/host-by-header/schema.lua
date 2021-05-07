06dc44c3d2b06931fe28d6ee38bfe80648cb0cf0
dhruv <dhruv@dream11.com>
2021-05-07 19:27:03 +0530

    ci: Add test cases

--- kong/plugins/host-by-header/schema.lua
+++ kong/plugins/host-by-header/schema.lua
@@ -1,51 +1,50 @@
 local typedefs = require "kong.db.schema.typedefs"
 
 return {
-	name = "host-by-header",
-	fields = {
-		{consumer = typedefs.no_consumer},
-		{protocols = typedefs.protocols_http},
-		{
-			config = {
-				type = "record",
-				fields = {
-					{
-						host = {
-							type = "string",
-                            default = "hostname-<PLACE_HOLDER>.com",
-                            required = true
-						}
-                    },
-					{
-						header_name = {
-							type = "string",
-                            len_min = 1,
-                            required = true
-						}
-                    },
-                    {
-						operation = {
-                            type = "string",
-                            len_min = 0,
-                            default = "none",
-                            one_of = {
-                                "none",
-                                "multiply",
-                                "add",
-                                "modulo",
-                            },
-                        },
-					},
-                    {
-						arithmetic_operand = {
-							type = "number",
-							default = 1
-						}
-                    },
-				},
-			}
-		}
-	},
-	entity_checks = {
-	}
+  name = "host-by-header",
+  fields = {
+    {consumer = typedefs.no_consumer},
+    {protocols = typedefs.protocols_http},
+    {
+      config = {
+        type = "record",
+        fields = {
+          {
+            host = {
+              type = "string",
+              default = "hostname-<PLACE_HOLDER>.com",
+              required = true
+            }
+          },
+          {
+            headers = {
+              type = "array",
+              default = {},
+              elements = { type = "string" },
+              required = true
+            }
+          },
+          {
+            operation = {
+              type = "string",
+              len_min = 0,
+              default = "none",
+              one_of = {
+                "none",
+                "modulo"
+              },
+            },
+          },
+          {
+            modulo_by = {
+              type = "number",
+              default = 1
+            }
+          },
+        },
+      }
+    }
+  },
+  entity_checks = {
+  }
 }
