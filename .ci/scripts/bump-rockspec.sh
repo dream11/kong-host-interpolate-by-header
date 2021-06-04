c56a6bd3eebc9a8a2aa8b43294577834c41de668
dhruv <dhruv@dream11.com>
2021-06-04 13:10:31 +0530

    ci: add git user to push

--- .ci/scripts/bump-rockspec.sh
+++ .ci/scripts/bump-rockspec.sh
@@ -15,6 +15,9 @@ sed -i.bak "s/$version/$new_version/g" $file_name && rm *.bak
 
 new_file_name="$prefix$new_version$suffix"
 
+git config user.name github-actions
+git config user.email github-actions@github.com
+
 git mv $file_name $new_file_name
 git remote set-url origin https://Dream11botpub:${commit_access_token}@github.com/dream11/kong-host-interpolate-by-header.git
 git add .
