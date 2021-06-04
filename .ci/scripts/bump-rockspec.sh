009f3e7df2c0560f5655ee2a4d359434b1fac932
dhruv <dhruv@dream11.com>
2021-06-04 13:23:55 +0530

    ci: git user to dream11botpub

--- .ci/scripts/bump-rockspec.sh
+++ .ci/scripts/bump-rockspec.sh
@@ -15,8 +15,8 @@ sed -i.bak "s/$version/$new_version/g" $file_name && rm *.bak
 
 new_file_name="$prefix$new_version$suffix"
 
-git config user.name github-actions
-git config user.email github-actions@github.com
+git config user.name Dream11botpub
+git config user.email Dream11botpub@github.com
 
 git mv $file_name $new_file_name
 git remote set-url origin https://Dream11botpub:${commit_access_token}@github.com/dream11/kong-host-interpolate-by-header.git
